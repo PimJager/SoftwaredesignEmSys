47e87b63786cc14c3a2e34070a41a0afc81b2771
pimjager <pim@pimjager.nl>
2015-09-17 15:39:18 +0200

    Ex03b: periodic

--- ex3/ex03b.c
+++ ex3/ex03b.c
@@ -2,6 +2,7 @@
 #include <signal.h>
 #include <unistd.h>
 #include <sys/mman.h>
+#include <stdint.h>
  
 #include <native/task.h>
 #include <native/timer.h>
@@ -20,25 +21,27 @@ int global = 0;
  
 void taskOne(void *arg)
 {
-    //increment and signal, wait for new signal
+    rt_task_set_periodic(NULL, TM_NOW, 1000000ULL);
     int i;
     for (i=0; i < ITER; i++)
     {
         rt_sem_p(&semGlobal, TM_INFINITE);
         rt_printf("I am taskOne and global = %d................\n", ++global);
         rt_sem_v(&semGlobal);
+        rt_task_wait_period(NULL);
     }
 }
  
 void taskTwo(void *arg)
 {
-    //wait and decrement
+    rt_task_set_periodic(NULL, TM_NOW, 1000000ULL);
     int i;
     for (i=0; i < ITER; i++)
     {
         rt_sem_p(&semGlobal, TM_INFINITE);
         rt_printf("I am taskTwo and global = %d----------------\n", --global);
         rt_sem_v(&semGlobal);
+        rt_task_wait_period(NULL);
     }
 }
  
@@ -50,7 +53,7 @@ int main(int argc, char* argv[])
     /* Avoids memory swapping for this program */
     mlockall(MCL_CURRENT|MCL_FUTURE);
 
-    int res = rt_sem_create(semGlobal, "binaryCount", 1, S_FIFO);
+    int res = rt_sem_create(&semGlobal, "binaryCount", 1, S_FIFO);
     if(res < 0) rt_printf("Failed to create semaphore; error: %d: %s", res, strerror(-res));
  
     /* create the two tasks */
@@ -60,6 +63,7 @@ int main(int argc, char* argv[])
     /* start the two tasks */
     rt_task_start(&t1, &taskOne, 0);
     rt_task_start(&t2, &taskTwo, 0);
- 
+
+    pause();
     return 0;
 }
