e12e2980f5e4e6012ceb031af90cc759338eed53
pimjager <pim@pimjager.nl>
2015-10-05 17:11:45 +0200

    Ex10 initial

---
+++ ex10/ex10a.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/mman.h>
+
+#include <native/task.h>
+#include <native/timer.h>
+#include <native/sem.h>
+
+#include  <rtdk.h>
+#include <sys/io.h>
+
+RT_TASK taskP;
+
+int runs = 0;
+RTIME diffs[NUMRUNS]; 
+
+#define BASEPERIOD  0   // baseperiod 0 to get ns
+
+#define PERIOD      1e6   // execution time of low prio task in ns
+
+#define NUMRUNS     1e5
+
+void task(void *arg)
+{
+    diffs[runs] = rt_timer_read();
+    int err = 0;
+    if(runs == 0) err = rt_task_set_periodic(NULL, TM_NOW, PERIOD);
+    if(err != 0)  rt_printf("scheduling task filed with err %d: %s\n", err), strerror(-err);
+    rt_printf("Run %d, runtime %d\n", runs, diffs[runs]);
+    runs++;
+}
+
+//startup code
+void startup(){
+  int err = 0;
+
+  // set timing to ns
+  rt_timer_set_mode(BASEPERIOD);
+
+  err = rt_task_create(&taskP, "high", 0, 50, 0);
+  if(err < 0) rt_printf("Failed to create task ; error: %d: %s", err, strerror(-err)); 
+    err = 0;
+  err = rt_task_start(&taskP, &task, 0);
+  if(err < 0) rt_printf("Failed to start task; error: %d: %s", err, strerror(-err)); 
+    err = 0;
+}
+
+void init_xenomai() {
+  /* Avoids memory swapping for this program */
+  mlockall(MCL_CURRENT|MCL_FUTURE);
+
+  /* Perform auto-init of rt_print buffers if the task doesn't do so */
+  rt_print_auto_init(1);
+}
+
+int main(int argc, char* argv[])
+{
+  printf("\nType CTRL-C to end this program\n\n" );
+
+  // code to set things to run xenomai
+  init_xenomai();
+
+  //startup code
+  startup();
+
+  // wait for CTRL-c is typed to end the program
+  pause();
+}
