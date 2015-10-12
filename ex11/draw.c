cd6252fe85badfd179bb2ab8c9926caa892bf27c
pimjager <pim@pimjager.nl>
2015-10-12 11:56:48 +0200

    Ex11 draw on right hand

--- ex11/draw.c
+++ ex11/draw.c
@@ -12,14 +12,15 @@
 
 #define CLK_INTR 7
 
-#define D0   1 //0x0000 0001
-#define D1   2 //0x0000 0010
-#define D2   4 //0x0000 0100
-#define D3   8 //0x0000 1000
-#define D4  16 //0x0001 0000
-#define D5  32 //0x0010 0000
-#define D6  64 //0x0100 0000
-#define D7 128 //0x1000 0000
+#define OFF   0 //0x0000 0000
+#define D0    1 //0x0000 0001
+#define D1    2 //0x0000 0010
+#define D2    4 //0x0000 0100
+#define D3    8 //0x0000 1000
+#define D4   16 //0x0001 0000
+#define D5   32 //0x0010 0000
+#define D6   64 //0x0100 0000
+#define D7  128 //0x1000 0000
 
 #define LEDS 0x378
 
@@ -30,34 +31,31 @@ RT_INTR clk_i;
 RTIME m[1000];
 
 void draw(){
-	int c = 0;
-	rt_printf("Start listening for intr\n");
-	while(c<1000){
-	    int x = rt_intr_wait(&clk_i, TM_INFINITE);
-	    if(x>0){
-	    	RTIME t = rt_timer_read();
-	    	rt_printf("INTR %d RECEVIED on %llu \n", c, t);
-	    	m[c] = t;
-	    	x--;
-	    	c++;
-	    }
+	while(1){
+		normalize();
+    	outb(D0 | D1 | D2 | D7, LEDS);
+    	rt_intr_wait(&clk_i, TM_INFINITE);
+    	outb(OFF, LEDS);
+    	rt_intr_wait(&clk_i, TM_INFINITE);
 	}
-	write_RTIMES(RESULTFILE, 1000, m);
-	rt_printf("Done listening, results written to %s", RESULTFILE);
 }
 
+//establishes the direction of the pendulum
+//0 for first interrupt is going left
+//1 for first interrupt is going right
+int direction(){
+	RTIME fst = rt_timer_read();
+	rt_intr_wait(&clk_i, TM_INFINITE);
+	RTIME snd = rt_timer_read();
+	RTIME diff = snd - fst;
+	if(diff < 6.6e7) return 0;
+	else 			 return 1;
+}
 
-void write_RTIMES(char * filename, unsigned int number_of_values,
-                  RTIME *time_values){
-         unsigned int n=0;
-         FILE *file;
-         file = fopen(filename,"w");
-         while (n<number_of_values) {
-              fprintf(file,"%u;%llu\n",n,time_values[n]);  
-              n++;
-         }
-         fclose(file);
- }
+void normalize(){
+	if(direction() == 0) rt_intr_wait(&clk_i, TM_INFINITE);
+	return;
+}
 
 void setupCLK(){
   	ioperm(0x37A, 1, 1);
